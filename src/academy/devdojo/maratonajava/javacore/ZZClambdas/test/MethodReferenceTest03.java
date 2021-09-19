6b4f4848ab4688cc864881a5403cd29a24ce40c3
cristianolopes1991 <cristianoizo803javase7@gmail.com>
2021-09-19 18:40:51 -0300

    Aprendendo programação funcional

---
+++ src/academy/devdojo/maratonajava/javacore/ZZClambdas/test/MethodReferenceTest03.java
@@ -0,0 +1,20 @@
+package academy.devdojo.maratonajava.javacore.ZZClambdas.test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+//Reference to an instance method of an arbitrary object of a particular type
+public class MethodReferenceTest03 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>(List.of("Rimuru", "veldora", "Hikimary"));
+        list.sort(String::compareTo);
+        System.out.println(list);
+
+        Function<String, Integer> numStringToInteger = Integer::parseInt;
+        Integer num = numStringToInteger.apply("10");
+        System.out.println(num);
+
+
+    }
+}
